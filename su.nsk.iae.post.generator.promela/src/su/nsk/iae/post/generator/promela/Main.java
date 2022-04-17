bfb721f34238b40038aeae1af12d9da860722f06
i.kondratev1 <i.kondratev1@g.nsu.ru>
2022-04-17 15:31:44 +0700

    added executable jar

--- su.nsk.iae.post.generator.promela/src/su/nsk/iae/post/generator/promela/Main.java
+++ su.nsk.iae.post.generator.promela/src/su/nsk/iae/post/generator/promela/Main.java
@@ -2,6 +2,8 @@ package su.nsk.iae.post.generator.promela;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -14,17 +16,25 @@ import su.nsk.iae.post.generator.promela.model.PromelaModel;
 import su.nsk.iae.post.poST.Model;
 
 public class Main {
+	private static String defaultInputPath = ".\\Sanitizer.post";
+	private static String defaultOutputPath = "D:\\Promela programms\\sanitizer-gen.pml";
+	
 	public static void main(String[] args) throws IOException {
-        Model m = prepareAndParseModelFromResource(".\\Sanitizer.post");
-        
+		String inputFile = getKeyValue(args, "-i").orElse(defaultInputPath);
+		String outputFile = getKeyValue(args, "-o").orElse(defaultOutputPath);
+		
+        Model m = prepareAndParseModelFromResource(inputFile);
         var res = new PromelaModel(m).toText();
 
         System.out.println(res);
-        
-        printToFile("D:\\Promela programms\\sanitizer-gen.pml", res);
-
-        System.out.println("Saved to file sanitizer-gen.pml.");
+        printToFile(outputFile, res);
+        System.out.println("Saved to \"" + outputFile + "\".");
     }
+	
+	private static Optional<String> getKeyValue(String[] args, String key) {
+		int pathIndex = List.of(args).indexOf(key) + 1;
+		return Optional.ofNullable(pathIndex > 0 && args.length > pathIndex ? args[pathIndex] : null);
+	}
 
     private static Model prepareAndParseModelFromResource(String fileName) throws IOException {
         Injector i = new PoSTStandaloneSetup().createInjectorAndDoEMFRegistration();
